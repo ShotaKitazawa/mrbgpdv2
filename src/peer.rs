13b242084a85966af16c28703c66ee55904611b7
ShotaKitazawa <skitazawa1121@gmail.com>
2022-12-18 17:25:18 +0900

    add test

---
+++ src/peer.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn peer_can_transition_to_connect_state() {
+        let config: Config = "64512 127.0.0.1 65413 127.0.0.2 active".parse().unwrap();
+        let mut peer = Peer::new(config);
+        peer.start();
+        peer.next().await;
+        assert_eq!(peer.state, State::Connect)
+    }
+}
